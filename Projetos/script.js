6144dce20f5ed0599fbf60919b4bf180172d4ea6
BrunoSalau <brunosalau@gmail.com>
2025-11-06 16:23:56 -0300

    Refactor animation functions and add qd3 interactions
    Refactored subir and descer functions to accept an element parameter. Added event listeners and cursor styles for qd3.

--- Projetos/script.js
+++ Projetos/script.js
@@ -1,27 +1,38 @@
+function subir(x){
+    x.style.transform = "translateY(-10px)"
+}
+function descer(x){
+    x.style.transform = "translateY(0px)"
+}
+
+//======================================================
+
 let qd1 = document.getElementById('qd1')
-let qd2 = document.getElementById('qd2')
-
-
-function subir1(){
-    qd1.style.transform = "translateY(-10px)"
-}
-function descer1(){
-    qd1.style.transform = "translateY(0px)"
-}
-function subir2(){
-    qd2.style.transform = "translateY(-10px)"
-}
-function descer2(){
-    qd2.style.transform = "translateY(0px)"
-}
 
 qd1.addEventListener('click', () => {
     window.location.href = "Mundo dos Peixes/index.html";
   });
 
+qd1.style.cursor = 'pointer'
+
+//======================================================
+
+let qd2 = document.getElementById('qd2')
+
 qd2.addEventListener('click', () => {
     window.location.href = "Tabuada/index.html";
   });
 
-qd1.style.cursor = 'pointer'
 qd2.style.cursor = 'pointer'
+
+//======================================================
+
+let qd3 = document.getElementById('qd3')
+
+qd3.addEventListener('click', () => {
+    window.location.href = "../index.html";
+  });
+
+qd3.style.cursor = 'pointer'
+
+//======================================================
